f23a8273d221b5d62e3fe5306a681213d9356a2a
cs-admin <chandras.appstech@gmail.com>
2025-02-26 17:12:46 -0500

    my app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, jsonify, render_template, request, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 
 
@@ -11,3 +12,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
+
+# app = Flask(__name__, static_folder='C:\09-GENAIWRK\BuildSomething\talktome-angular-app\dist\talktome-angular-app')
+# Serve Angular application
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True)
